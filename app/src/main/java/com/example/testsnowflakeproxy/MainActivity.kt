cdfe1042bc25f388cf5534f7ac8e165425b9d3d5
b <dsnake@protonmail.com>
2021-12-13 18:40:57 -0500

    sample app work for the day

--- app/src/main/java/com/example/testsnowflakeproxy/MainActivity.kt
+++ app/src/main/java/com/example/testsnowflakeproxy/MainActivity.kt
@@ -4,44 +4,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.io.File
 
-import IPtProxy.IPtProxy
+import IPtProxy.IPtProxy;
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var count: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val cacheDir = File(cacheDir, "pt")
-        if (!cacheDir.exists()) cacheDir.mkdir()
+        val stateLocation = File(cacheDir, "pt")
+        if (!stateLocation.exists()) stateLocation.mkdir()
 
-        IPtProxy.setStateLocation(cacheDir.absolutePath)
+        IPtProxy.setStateLocation(stateLocation.absolutePath)
 
+        // android activity config
         val start = findViewById<Button>(R.id.start)
         val stop = findViewById<Button>(R.id.stop)
         val label = findViewById<TextView>(R.id.label)
 
-        val capacity = 1
-        val broker = "https://snowflake-broker.bamsoftware.com/"
-        val relay = "wss://snowflake.bamsoftware.com/"
-        val stun = "stun:stun.stunprotocol.org:3478"
+        // snowflake proxy config
+        val capacity : Long = 0
+        // all String args are null to use defaults included in IPtProxy
+        val broker = null
+        val relay = null
+        val stun = null
         val logFile: String? = null
-        val keepLocalAddresses = true
-        val unsafeLogging = false
-
-        var count = 0
+        val keepLocalAddresses = false
+        val unsafeLogging = true
 
         start.setOnClickListener {
-            IPtProxy.startSnowflakeProxy(capacity.toLong(), broker, relay, stun, logFile, keepLocalAddresses, unsafeLogging) {
+            IPtProxy.startSnowflakeProxy(capacity, broker, relay, stun, logFile, keepLocalAddresses, unsafeLogging) {
                 count++
-                label.setText("Update: " + count)
+                Toast.makeText(this, "Client connected", Toast.LENGTH_LONG).show()
+                label.text = "Connected Clients: $count"
             }
         }
 
         stop.setOnClickListener {
             IPtProxy.stopSnowflakeProxy()
         }
-
     }
 }
